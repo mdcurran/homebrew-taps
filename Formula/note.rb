c1333de9f0e36a6852963b2b40a4a7a219ba3ac8
Max Curran <maxcurran96@gmail.com>
2022-12-19 13:07:13 +0000

    note v0.2.0

--- Formula/note.rb
+++ Formula/note.rb
@@ -1,8 +1,8 @@
 class Note < Formula
   desc "Manage text files from the command line"
   homepage "https://github.com/mdcurran/note"
-  url "https://github.com/mdcurran/note/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b4bd6facf3d77dd3540528427d236fe96b28968c972263d84bb2df8a556d9b95"
+  url "https://github.com/mdcurran/note/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c902fdbe5444eec1458b83164854727309687f93259f3d941b1588e58593590a"
 
   depends_on "go" => :build
 
